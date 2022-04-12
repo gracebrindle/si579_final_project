24561d7fd4ce1566258868ed90f002e284ee9b90
christoarmani <christo296@hotmail.com>
2022-04-12 11:36:49 -0400

    Added initial Bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <main className="container">
+      <h1 className="row">Recipe Finder</h1>
+      <div className="row">
+          <div className="col">Saved words: <span></span></div>
+      </div>
+      <div className="row">
+          <div className="input-group col">
+              <input className="form-control" type="text" placeholder="Enter a word" id="word_input" />
+          </div>
+      </div>
+      <div className="row">
+          <h2 className="col" id="output_description"></h2>
+      </div>
+      <div className=".col-auto">
+          <output className="col"></output>
+          <button type="button" class="btn btn-outline-secondary">Reset</button>
+          <button type="button" className="btn btn-primary">Search</button>
+      </div>
+  </main>
   );
 }
 
