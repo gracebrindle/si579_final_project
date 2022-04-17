00749f71b617a7bfd692bb30e74d063b6a96faef
christoarmani <christo296@hotmail.com>
2022-04-17 14:19:28 -0400

    Cleaned up App.js from placeholder RecipeCards

--- src/App.js
+++ src/App.js
@@ -53,38 +53,12 @@ function App() {
       <div className="row row-cols-3">
           <div className="col">
             <CardSkeleton />
+            </div>
+            <div className="col">
             <RecipeCard />
-
             </div>
             <div className="col">
-            <div className="card">
-              <img src="https://assets.bonappetit.com/photos/62154b9846c5f5cb1103c4f0/3:4/w_748%2Cc_limit/20220215%2520Vegan%2520Vodka%2520Pasta_LEDE.jpg"
-                className="card-img-top" alt="..."/>
-              <div className="card-body">
-              <h5 className="card-title">Recipe title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <p>Ready in Minutes</p>
-                <a href="#" className="btn btn-primary">View Recipe</a>
-              </div>
-            </div>
-            </div>
-            <div className="col">
-            <div className="card">
-              <img src="https://post.healthline.com/wp-content/uploads/2018/04/steak-meat-1200x628-facebook-1200x628.jpg"
-                className="card-img-top" alt="..."/>
-              <div className="card-body">
-              <h5 className="card-title">Recipe title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                  <p>Ready in Minutes</p>
-                <a href="#" className="btn btn-primary">View Recipe</a>
-              </div>
-            </div>
+            <RecipeCard />
             </div>
         </div>
       </div>
