05134c1446eb5bee63c33dee0fddf636f855d3b9
Grace Brindle <89871638+gracebrindle@users.noreply.github.com>
2022-04-19 23:30:30 -0400

    Update InputGroup.js

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -14,6 +14,7 @@ const InputGroup = (props) => {
     setNoResults,
   } = props;
 
+  // Add individual ingredient
   function addIngredient(ingredient) {
     setIncludeIngredientsValue((currentIngredients) => {
       return [
